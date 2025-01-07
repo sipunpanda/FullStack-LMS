c1c013cecdae3493fc264a5e332d0bbbe127828f
sipunpanda <sipunpanda26@gmail.com>
2025-01-07 23:49:55 +0530

    Added page not found

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './Pages/HomePage.jsx'
 import AboutUs from './Pages/AboutUs.jsx'
+import NotFound from './Pages/NotFound.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
    <Routes>
     <Route path='/' element ={<HomePage /> } > </Route>
     <Route path='/about' element ={<AboutUs /> } > </Route>
+    <Route path='*' element ={<NotFound />} > </Route>
     </Routes>
     </>
   )
