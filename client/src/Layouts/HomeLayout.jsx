79c366c08d44098faad7ac4b513e6f08da06073a
sipunpanda <sipunpanda26@gmail.com>
2025-01-12 23:08:33 +0530

    toastPromise Functionality debug in authSlice

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -4,6 +4,7 @@ import { AiFillCloseCircle } from 'react-icons/ai'
 import Footer from '../Components/Footer';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../Redux/Slices/AuthSlice';
 
 function HomeLayout({ children }) {
 
@@ -25,18 +26,20 @@ const role = useSelector((state) => state?.auth?.role)
         const element = document.getElementsByClassName('drawer-toggle');
         element[0].checked = false;
 
-        const drawerSide = document.getElementById('drawer-side');
+        const drawerSide = document.getElementsByClassName('drawer-side');
         drawerSide[0].style.width = 0;
     }
 
-    function handleLogout(e){
+    async function handleLogout(e){
+
         e.preventDefault();
 
-        // const res = await dispatch(logout());
+        const res = await dispatch(logout());
 
-    //    if (res?.payload?.success) {
-    //      navigate("/")
-    //    }
+   
+       if (res?.payload?.success) {
+         navigate("/")
+       }
     }
 
     return (
@@ -106,6 +109,7 @@ const role = useSelector((state) => state?.auth?.role)
 
                             {isLoggedIn && (
                                 <li className="absolute bottom-4 w-[90%]">
+                                    
 
                                 <div className="w-full flex items-center justify-center">
                                     <button className='btn btn-primary btn-sm font-semibold rounded-md w-[50%]'>
--- client/src/Redux/Slices/AuthSlice.js
+++ client/src/Redux/Slices/AuthSlice.js
@@ -12,26 +12,26 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
     try {
         console.log("hi",data);
         
-        const res = await axiosInstance.post('user/register', data);
+        const res = axiosInstance.post('user/register', data);
         console.log("hi2",res);
-        // toast.promise(
-        //     res, {
-        //     loading: "Wait creating Your Account",
-        //    success: (response) => {
-        //             console.log("Account created successfully:", response.data);
-        //             return response.data.message || "Account created successfully!";
-        //         },
-        //     error: "Could not create your account"
-        // }
-        // );
-        if(res){
-            toast.success("Account created successfully");
+        toast.promise(
+            res, {
+            loading: "Wait creating Your Account",
+           success: (response) => {
+                    console.log("Account created successfully:", response.data);
+                    return response.data.message || "Account created successfully!";
+                },
+            error: "Could not create your account"
         }
-        console.log("hi3",res);
+        );
+        // if(res){
+        //     toast.success("Account created successfully");
+        // }
+        // console.log("hi3",res);
 
         return (await res).data;
     } catch (error) {
-        console.log("hi4",res);
+        // console.log("hi4",res);
 
         toast.error(error?.response?.data?.message)
     }
@@ -40,28 +40,60 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
 
 export const login = createAsyncThunk("/auth/login", async (data) => {
     try {
-        console.log("hi",data);
+        // console.log("hi",data);
        
         
-        const res = await axiosInstance.post('user/login', data);
+        const res =  axiosInstance.post('user/login', data);
         // console.log("hi2",res);
-        // await toast.promise(
-        //     res, {
-        //     loading: "Wait authentication in progress...",
-        //     success: (data) => {
-        //         console.log("data",data);
+        await toast.promise(
+            res, {
+            loading: "Wait authentication in progress...",
+            success: (data) => {
+                // console.log("data",data);
                 
-        //         return data?.data?.message;
-        //     },
-        //     error: "Failed to log in"
-        // }
-        // );
-        toast.success( "Account successfully logged in");
-        return res.data;
+                return data?.data?.message;
+            },
+            error: "Failed to log in"
+        }
+        );
+        // toast.success( "Account successfully logged in");
+        return (await res).data;
     } catch (error) {
-        toast.error("end ho gaya")
+        toast.error(error?.response?.data?.message)
     }
-})
+});
+
+export const logout = createAsyncThunk("/auth/logout", async () => { 
+    // console.log("inlogout1");
+
+    try {
+        // console.log("hi");
+       
+        
+        const res = axiosInstance.get('user/logout');
+        // console.log("hi2",res);
+        await toast.promise(
+            res, {
+            loading: "Wait authentication in progress...",
+            success: (data) => {
+                // console.log("data",data);
+                
+                return data?.data?.message;
+            },
+            error: "Failed to logout"
+        }
+        );
+        toast.success( "Account Logout successfully");
+        return (await res).data;
+    } catch (error) {
+        // console.log("inlogout error",error);
+
+        toast.error(error?.response?.data?.message)
+    }
+}
+);
+
+
 
 
 const authSlice = createSlice({
@@ -69,13 +101,20 @@ const authSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers:(builder) =>{
-        builder.addCase(login.fulfilled, (state, action) =>{
+        builder
+        .addCase(login.fulfilled, (state, action) =>{
             localStorage.setItem("data", JSON.stringify(action?.payload?.user));
             localStorage.setItem("isLoggedIn", true);
             localStorage.setItem("role", action?.payload?.user?.role);
             state.data = action?.payload?.user;
             state.isLoggedIn = true;
         })
+        .addCase(logout.fulfilled, (state, action) => {
+            localStorage.clear();
+            state.data = {};
+            state.isLoggedIn = false;
+            state.role = "";
+    })
     }
 })
 
